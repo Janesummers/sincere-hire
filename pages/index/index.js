8485bff1caa63b666fde813e628e35fc3a22af75
janesummer <1752321720@qq.com>
2020-01-14 22:30:11 +0800

    更新首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    station: [
+      {position: '理财规划师', location: '漳州', people: '4', companyType: '国企', company: '中信建投期货', price: '面议', time: '19-09-30'},
+      {position: '电气工程师', location: '漳州', people: '1', companyType: '上市公司', company: '新旗滨', price: '面议', time: '今天'},
+      {position: '软件开发', location: '北京', people: '2', companyType: '国企', company: '北京中电普华信息技术有限公司', price: '面议', time: '19-10-23'},
+      {position: '人力资源实习', location: '南京', people: '若干', companyType: '上市公司', company: '好未来励步事业部', price: '面议', time: '19-10-11'}
+    ],
+    hotSubject: [
+      {icon: 'icon-hangkonghangtian-aerospace-diqiuyueqiu- yellow', text: '工学'}, 
+      {icon: 'icon-qianbi blue', text: '理学'},
+      {icon: 'icon-ico_home_obligation orange', text: '经济学'},
+      {icon: 'icon-yezi- blue', text: '农学'},
+      {icon: 'icon-huodongguanli blue', text: '管理学'},
+      {icon: 'icon-yiyuanguahao orange', text: '医学'},
+      {icon: 'icon-shuben yellow', text: '文学'},
+      {icon: 'icon-bingtutongji orange', text: '法学'},
+      {icon: 'icon-jiaoxue orange', text: '教育学'},
+      {icon: 'icon-yishu yellow', text: '艺术学'},
+      {icon: 'icon-gongshang blue', text: '工商管理'},
+    ]
   },
 
   /**
