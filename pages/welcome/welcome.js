5134037ac96095de8196bbfa8f3dced95135adae
janesummer <1752321720@qq.com>
2020-01-21 17:37:12 +0800

    修复bug

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -56,7 +56,7 @@ Page({
           }
         }
       })
-    }
+    } 
 
     isUserInfo();
 
@@ -145,6 +145,9 @@ Page({
     let unionid = wx.getStorageSync('unionid');
     let rule = wx.getStorageSync('rule');
     console.log(unionid, rule)
+    wx.switchTab({
+      url: '/pages/index/index'
+    })
   },
 
   showBox (e) {
