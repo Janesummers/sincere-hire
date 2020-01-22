bfe1f2702af0a277c3a1c276f36f71df422d2c27
janesummer <1752321720@qq.com>
2020-01-22 16:45:39 +0800

    完成招聘列表布局

--- pages/job-list/job-list.js
+++ pages/job-list/job-list.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    station: [
+      {position: '理财规划师', location: '漳州', people: '4', companyType: '国企', company: '中信建投期货', price: '面议', time: '19-09-30'},
+      {position: '电气工程师', location: '漳州', people: '1', companyType: '上市公司', company: '新旗滨', price: '面议', time: '今天'},
+      {position: '软件开发', location: '北京', people: '2', companyType: '国企', company: '北京中电普华信息技术有限公司', price: '面议', time: '19-10-23'},
+      {position: '人力资源实习', location: '南京', people: '若干', companyType: '上市公司', company: '好未来励步事业部', price: '面议', time: '19-10-11'}
+    ]
   },
 
   /**
@@ -62,5 +67,11 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  toJobDetail () {
+    wx.navigateTo({
+      url: '/pages/job-detail/job-detail'
+    })
   }
 })
