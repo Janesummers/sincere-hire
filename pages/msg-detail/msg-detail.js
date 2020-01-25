08ef824a7d50fc0dd24839d984e6310256081016
janesummer <1752321720@qq.com>
2020-01-25 15:10:52 +0800

    完成聊天详情的部分逻辑

--- pages/msg-detail/msg-detail.js
+++ pages/msg-detail/msg-detail.js
@@ -5,19 +5,118 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    height: '',
+    originHeight: 0,
+    top: 0,
+    keyHeight: 0,
+    toLast: 'item',
+    list: [
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'self'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'self'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'self'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'self'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'other'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。',
+        origin: 'self'
+      },
+      {
+        img: '',
+        text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。1',
+        origin: 'other'
+      }
+    ],
+    sendText: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.createSelectorQuery().select('.list').boundingClientRect(function(rect){
-      // 使页面滚动到底部
-      wx.pageScrollTo({
-        scrollTop: rect.bottom
+    // console.log(height = wx.getSystemInfoSync().windowHeight)
+    let height = wx.getSystemInfoSync().windowHeight - 50;
+    console.log(height)
+    this.setData({
+      height,
+      originHeight: height
+    }, () => {
+      this.setData({
+        toLast: `item${this.data.list.length - 1}`
       })
-    }).exec()
+      setTimeout(() => {
+        let list = this.data.list;
+        list.push({
+          img: '',
+          text: '您的简历已放入我公司人才库，若有需要我们会与您联系，感谢关注。2',
+          origin: 'other'
+        })
+        this.setData({
+          list
+        }, () => {
+          this.setData({
+            toLast: `item${this.data.list.length - 1}`
+          })
+        })
+      }, 3000);
+    })
+    wx.onKeyboardHeightChange(res => {
+      if (res.height != 0 && res.height > parseInt(this.data.keyHeight)) {
+        console.log(res)
+        let height = parseInt(this.data.height);
+        height = height - parseInt(res.height) - 50
+        this.setData({
+          height,
+          keyHeight: res.height,
+          top: res.height
+        })
+      }
+    })
   },
 
   /**
@@ -67,5 +166,61 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  test () {
+    console.log("唤起")
+    let top = this.data.keyHeight
+    let height = parseInt(this.data.height);
+    height = height - parseInt(top)
+    this.setData({
+      height,
+      top,
+      toLast: 'item'
+    }, () => {
+      setTimeout(() => {
+        this.setData({
+          toLast: `item${this.data.list.length - 1}`
+        })
+      }, 50);
+    })
+    
+  },
+
+  end () {
+    console.log("收")
+    let height = parseInt(this.data.originHeight);
+    this.setData({
+      height,
+      top: 0,
+      toLast: 'item'
+    }, () => {
+      this.setData({
+        toLast: `item${this.data.list.length - 1}`
+      })
+    })
+  },
+
+  bindconfirm (e) {
+    let list = this.data.list;
+    list.push({
+      img: '',
+      text: e.detail.value,
+      origin: 'self'
+    })
+    this.setData({
+      list
+    }, () => {
+      this.setData({
+        toLast: `item${this.data.list.length - 1}`,
+        sendText: ''
+      })
+    })
+  },
+
+  adInputChange (e) {
+    this.setData({
+      sendText: e.detail.value
+    })
   }
 })
